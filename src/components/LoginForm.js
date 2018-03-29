90ec2f4aeaedc72bba9a4649e48bd8f72922fb5e
jkzasori <jochitoel1@hotmail.com>
2018-03-28 20:10:44 -0500

    Don't Mutate that State

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
-	onChangeText(text) {
+	onEmailChange(text) {
 		this.props.emailChanged(text);
 	}
+	onPasswodChange(text) {
+		this.props.passwordChanged(text);
+	}
+	onButtonPress() {
+		const { email, password } = this.props;
+
+		this.props.loginUser({ email, password})
+	}
+	renderError() {
+		if (this.props.error) {
+			return (
+				<View style={{ backgroundColor: 'white'}}>
+					<Text style={styles.errorTextStyle}>
+						{this.props.error}
+					</Text>
+				</View>
+				)
+		}
+	}
+	renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    }
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    );
+  }
 	render() {
 		return(
 			<Card>
@@ -15,6 +45,7 @@ class LoginForm extends Component {
 						label="Email"
 						placeholder="email@gmail.com"
 						onChangeText={this.onEmailChange.bind(this)}
+						value={this.props.email}
 					/>
 				</CardSection>
 
@@ -23,17 +54,35 @@ class LoginForm extends Component {
 						secureTextEntry
 						label="Password"
 						placeholder="Password"
+						onChangeText={this.onPasswodChange.bind(this)}
+						value={this.props.password}
 					/>
 				</CardSection>
-
+					{this.renderError()}
 				<CardSection>
-					<Button>
-						Login
-					</Button>
+					{this.renderButton()}
 				</CardSection>
 			</Card>
 			)
 	}
 }
 
-export default connect(null,, emailChanged)(LoginForm);
+const styles = StyleSheet.create({
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+})
+
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error, loading } = auth;
+
+  return { email, password, error, loading };
+};
+
+export default connect(mapStateToProps, { 
+	emailChanged, 
+	passwordChanged,
+	loginUser
+})(LoginForm);
